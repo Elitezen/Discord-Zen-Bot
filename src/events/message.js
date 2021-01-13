d7791f26edb73034c0886236c8924325e7e16a5d
Elitezen <61894124+Elitezen@users.noreply.github.com>
2021-01-12 20:23:33 -0500

    Added Message Event, Set Up Command Handling

--- src/events/message.js
+++ src/events/message.js
@@ -1,5 +1,19 @@
-const Discord = require('discord.js');
-
 module.exports = (Bot, Message) => {
-    // Handle Message
+    let prefix = '>';
+
+    const msgIsDM = Message.channel.type === 'dm';
+    const authorIsBot = Message.author.bot;
+    const notForZen = !Message.content.startsWith(prefix);
+    if (msgIsDM || authorIsBot || notForZen) return;
+
+    const args = Message.content.slice(prefix.length).trim().split(/ +/);
+    const commandToGet = args.shift().toLowerCase();
+    if (!Bot.commands.has(commandToGet)) return;
+
+    try {
+        Bot.commands.get(commandToGet).execute(Bot, Message, args);
+    } catch (err) {
+        console.error(err);
+        Message.reply('An Error Has Occured');
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const Discord = require('discord.js');
+const fs = require('fs');
 const Bot = new Discord.Client();
 const loadEvents = require('../src/handlers/event.js');
 
 Bot.events = new Discord.Collection();
+Bot.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('../Discord-Zen-Bot/src/commands/').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`../src/commands/${file}`);
+    Bot.commands.set(command.name, command);
+}
 
 const load = async() => await loadEvents.run(Bot);
 
